4138f770e806945e6ad6780529487badd11432ab
Sebastian Callejas <scallejas18051@columbushs.com>
2018-03-06 08:05:27 -0500

    completed in class on 3/6/2018

--- CreatingClasses/src/BankAccount/BankAccount.java
+++ CreatingClasses/src/BankAccount/BankAccount.java
@@ -12,7 +12,36 @@ package BankAccount;
  */
 public class BankAccount {
 //Lets get it done
-    private String balance;
-    private String userName;
-    private String passWord;
+    private double balance;
+    private String username;
+    private String password;
+    
+    public BankAccount(double bal, String user, String pass){
+        balance = bal;
+        username = user;
+        password = pass;
+    }
+    
+    public double getBalance(){
+        return balance;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setBalance(double pBalance){
+        balance = pBalance;
+    }
+    public void setUsername(String pUsername){
+        username = pUsername;
+    }
+    public void setPassword(String pPassword){
+        password = pPassword;
+    }
+    public String toString(){
+        return "Account balance is "+balance+". Username is "+username+
+                ". Password is "+password+".";
+    }
 }
--- CreatingClasses/src/Student/Student.java
+++ CreatingClasses/src/Student/Student.java
@@ -12,7 +12,41 @@ package Student;
  */
 public class Student {
 //Lets get it done
-    int id;
-    int gpa;
-    int gradeLevel;
+    private int id;
+    private int gpa;
+    private int gradeLevel;
+    
+    public Student(int i, int g, int gL){
+        id = i;
+        gpa = g;
+        gradeLevel = gL;
+    }
+    
+    public int getID(){
+        return id;
+    }
+    
+    public int getGPA(){
+        return gpa;
+    }
+    
+    public int getGradeLevel(){
+        return gradeLevel;
+    }
+    
+    public void setID(int pID){
+        id = pID;
+    }
+    
+    public void setGPA(int pGPA){
+        gpa =pGPA;
+    }
+    
+    public void setGradeLevel(int pGradeLevel){
+        gradeLevel = pGradeLevel;
+    }
+    public String toString(){
+        return "Student ID number is: "+id+". Grade level: "+gradeLevel+
+                ". GPA: "+gpa+".";
+    }
 }
